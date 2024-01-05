2b5dc6214d0981178ac336985dd8476f3bc500b7
CRISTIANRAMIREZC <cristiancrc97@hotmail.com>
2024-01-05 18:22:35 -0500

    ssetting up project structure with Clean Architecture

--- app/src/main/java/com/example/algarweatherapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/algarweatherapp/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.algarweatherapp
+package com.example.algarweatherapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.algarweatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
